576fde16844c0fe8d00262424307e5480a9cc656
Ibrahim Chaehoi <ichaehoi@gmail.com>
2014-02-23 12:01:26 +0000

    Fix issue #270 : Allow user to define custom Jawr processor/generator using a property file in META-INF from a jar Fix issue #271 : Jawr doesn't handle properly bundle dependencies in Closure compiler arguments Fix issue #273 ResourceBundleMessagesGenerator does not add quotes around property message keys
    git-svn-id: https://svn.java.net/svn/jawr~svn/core/trunk@1070 4db09855-0bbe-6bc9-8408-d5225a1fabd9

--- jawr-core/src/main/java/net/jawr/web/resource/bundle/global/postprocessor/google/closure/ClosureGlobalPostProcessor.java
+++ jawr-core/src/main/java/net/jawr/web/resource/bundle/global/postprocessor/google/closure/ClosureGlobalPostProcessor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2013 Ibrahim Chaehoi
+ * Copyright 2011-2014 Ibrahim Chaehoi
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
