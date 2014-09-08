65d6e311f2ed2157c219672952ea131e4731ded6
Ibrahim Chaehoi <ichaehoi@gmail.com>
2014-09-08 14:17:21 +0200

    Use RhinoEngine for coffeescript

--- jawr-core/src/main/java/net/jawr/web/resource/bundle/generator/js/coffee/CoffeeScriptGenerator.java
+++ jawr-core/src/main/java/net/jawr/web/resource/bundle/generator/js/coffee/CoffeeScriptGenerator.java
@@ -16,11 +16,9 @@ package net.jawr.web.resource.bundle.generator.js.coffee;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,8 +33,8 @@ import net.jawr.web.resource.bundle.generator.GeneratorContext;
 import net.jawr.web.resource.bundle.generator.PostInitializationAwareResourceGenerator;
 import net.jawr.web.resource.bundle.generator.resolver.ResourceGeneratorResolver;
 import net.jawr.web.resource.bundle.generator.resolver.ResourceGeneratorResolverFactory;
+import net.jawr.web.util.js.RhinoEngine;
 
-import org.mozilla.javascript.Context;
 import org.mozilla.javascript.JavaScriptException;
 import org.mozilla.javascript.Scriptable;
 
@@ -45,7 +43,9 @@ import org.mozilla.javascript.Scriptable;
  * 
  * @author ibrahim Chaehoi
  */
-public class CoffeeScriptGenerator extends AbstractJavascriptGenerator implements ConfigurationAwareResourceGenerator, PostInitializationAwareResourceGenerator, ICoffeeScriptGenerator {
+public class CoffeeScriptGenerator extends AbstractJavascriptGenerator
+		implements ConfigurationAwareResourceGenerator,
+		PostInitializationAwareResourceGenerator, ICoffeeScriptGenerator {
 
 	/** The coffee script suffix */
 	private static final String COFFEE_SCRIPT_SUFFIX = "coffee";
@@ -61,137 +61,136 @@ public class CoffeeScriptGenerator extends AbstractJavascriptGenerator implement
 
 	/** The resolver */
 	private ResourceGeneratorResolver resolver;
-	
-	/** The JS global scope */
-	private Scriptable globalScope;
-	
+
 	/** The jawr config */
 	private JawrConfig config;
-	
+
 	/** The coffee script options */
 	private String options;
-	
+
+	/** The Rhino engine */
+	private RhinoEngine rhino;
+
 	/**
-	 * Constructor 
+	 * Constructor
 	 */
 	public CoffeeScriptGenerator() {
-		resolver = ResourceGeneratorResolverFactory.createSuffixResolver(COFFEE_SCRIPT_SUFFIX);
+		resolver = ResourceGeneratorResolverFactory
+				.createSuffixResolver(COFFEE_SCRIPT_SUFFIX);
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.bundle.generator.ConfigurationAwareResourceGenerator#setConfig(net.jawr.web.config.JawrConfig)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.bundle.generator.ConfigurationAwareResourceGenerator
+	 * #setConfig(net.jawr.web.config.JawrConfig)
 	 */
 	public void setConfig(JawrConfig config) {
 		this.config = config;
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.bundle.generator.PostInitializationAwareResourceGenerator#afterPropertiesSet()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.jawr.web.resource.bundle.generator.
+	 * PostInitializationAwareResourceGenerator#afterPropertiesSet()
 	 */
 	public void afterPropertiesSet() {
-		
-		options = config.getProperty(JAWR_JS_GENERATOR_COFFEE_SCRIPT_OPTIONS, "");
-		
+
+		options = config.getProperty(JAWR_JS_GENERATOR_COFFEE_SCRIPT_OPTIONS,
+				"");
+
 		// Load JavaScript Script Engine
-		String script = config.getProperty(JAWR_JS_GENERATOR_COFFEE_SCRIPT_LOCATION, DEFAULT_COFFEE_SCRIPT_JS_LOCATION);
+		String script = config.getProperty(
+				JAWR_JS_GENERATOR_COFFEE_SCRIPT_LOCATION,
+				DEFAULT_COFFEE_SCRIPT_JS_LOCATION);
+		rhino = new RhinoEngine();
 		InputStream inputStream = getResourceInputStream(script);
-        try {
-            try {
-                Reader reader = new InputStreamReader(inputStream, "UTF-8");
-                try {
-                    Context context = Context.enter();
-                    context.setOptimizationLevel(-1); // Without this, Rhino hits a 64K bytecode limit and fails
-                    try {
-                        globalScope = context.initStandardObjects();
-                        context.evaluateReader(globalScope, reader, "coffee-script.js", 0, null);
-                    } finally {
-                        Context.exit();
-                    }
-                } finally {
-                	IOUtils.close(reader);
-                }
-            } catch (UnsupportedEncodingException e) {
-                throw new BundlingProcessException(e); // This should never happen
-            } finally {
-                IOUtils.close(inputStream);
-            }
-        } catch (IOException e) {
-            throw new BundlingProcessException(e); // This should never happen
-        }
+		rhino.evaluate("coffee-script.js", inputStream);
 	}
-	
+
 	/**
 	 * Returns the resource input stream
-	 * @param path the resource path
+	 * 
+	 * @param path
+	 *            the resource path
 	 * @return the resource input stream
 	 */
 	private InputStream getResourceInputStream(String path) {
 		InputStream is = config.getContext().getResourceAsStream(path);
-		if(is == null){
+		if (is == null) {
 			try {
 				is = ClassLoaderResourceUtils.getResourceAsStream(path, this);
 			} catch (FileNotFoundException e) {
 				throw new BundlingProcessException(e);
 			}
 		}
-		
+
 		return is;
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.bundle.generator.BaseResourceGenerator#getResolver()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.bundle.generator.BaseResourceGenerator#getResolver
+	 * ()
 	 */
 	public ResourceGeneratorResolver getResolver() {
 		return resolver;
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.bundle.generator.TextResourceGenerator#createResource(net.jawr.web.resource.bundle.generator.GeneratorContext)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.bundle.generator.TextResourceGenerator#createResource
+	 * (net.jawr.web.resource.bundle.generator.GeneratorContext)
 	 */
 	public Reader createResource(GeneratorContext context) {
-		
+
 		String path = context.getPath();
 		Reader rd = null;
 		try {
 			List<Class<?>> excluded = new ArrayList<Class<?>>();
 			excluded.add(ICoffeeScriptGenerator.class);
-			rd = context.getResourceReaderHandler().getResource(path, false, excluded);
+			rd = context.getResourceReaderHandler().getResource(path, false,
+					excluded);
 			StringWriter swr = new StringWriter();
 			IOUtils.copy(rd, swr);
-			
+
 			String result = compile(swr.toString());
 			rd = new StringReader(result);
-			
+
 		} catch (ResourceNotFoundException e) {
 			throw new BundlingProcessException(e);
 		} catch (IOException e) {
 			throw new BundlingProcessException(e);
 		}
-		
+
 		return rd;
 	}
-	
+
 	/**
-	 * Compile the CoffeeScript source to a JS source 
-	 * @param coffeeScriptSource the CoffeeScript source
-	 * @return the JS source 
+	 * Compile the CoffeeScript source to a JS source
+	 * 
+	 * @param coffeeScriptSource
+	 *            the CoffeeScript source
+	 * @return the JS source
 	 */
-	public String compile (String coffeeScriptSource) {
-        
-		Context context = Context.enter();
-        try {
-        	
-            Scriptable compileScope = context.newObject(globalScope);
-            compileScope.setParentScope(globalScope);
-            compileScope.put("coffeeScriptSource", compileScope, coffeeScriptSource);
-            try {
-                return (String)context.evaluateString(compileScope, String.format("CoffeeScript.compile(coffeeScriptSource, '%s');", options),
-                        "JCoffeeScriptCompiler", 0, null);
-            } catch (JavaScriptException e) {
-                throw new BundlingProcessException(e);
-            }
-        } finally {
-            Context.exit();
-        }
-    }
+	public String compile(String coffeeScriptSource) {
+
+		Scriptable compileScope = rhino.newObject();
+		compileScope
+				.put("coffeeScriptSource", compileScope, coffeeScriptSource);
+		try {
+			return (String) rhino.evaluateString(compileScope,
+					String.format(
+							"CoffeeScript.compile(coffeeScriptSource, '%s');",
+							options), "JCoffeeScriptCompiler");
+		} catch (JavaScriptException e) {
+			throw new BundlingProcessException(e);
+		}
+	}
 }
