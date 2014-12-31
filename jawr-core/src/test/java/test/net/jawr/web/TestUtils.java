ce2a39967ec9334b4325c8fdb3a687bddbd6cbf3
Ibrahim Chaehoi <ichaehoi@gmail.com>
2014-12-31 17:02:26 +0100

    Fix issue #323 - Add TestUtils class
    Fix line ending issue

---
+++ jawr-core/src/test/java/test/net/jawr/web/TestUtils.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2014 Ibrahim Chaehoi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package test.net.jawr.web;
+
+import net.jawr.web.util.StringUtils;
+
+import org.junit.Assert;
+
+/**
+ * @author Ibrahim Chaehoi
+ */
+public class TestUtils {
+
+	/**
+	 * Assert that two contents are equals without taking in account the line feed character.
+	 * @param msg the message
+	 * @param expected the expected String to check
+	 * @param actual the actual String to check
+	 */
+	public static void assertContentEquals(String msg, String expected, String actual){
+		Assert.assertEquals(msg, StringUtils.normalizeLineFeed(expected), StringUtils.normalizeLineFeed(actual));
+	}
+}
