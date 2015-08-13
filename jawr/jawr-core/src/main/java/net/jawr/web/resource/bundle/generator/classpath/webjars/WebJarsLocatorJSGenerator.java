5d54a68d5c04b5bd9df837093a97721e878ffc87
Ibrahim Chaehoi <ichaehoi@gmail.com>
2015-08-13 11:18:00 +0200

    Fix issue JAWR-349 Allow user to use short reference for webjars resources instead of full one

--- jawr/jawr-core/src/main/java/net/jawr/web/resource/bundle/generator/classpath/webjars/WebJarsLocatorJSGenerator.java
+++ jawr/jawr-core/src/main/java/net/jawr/web/resource/bundle/generator/classpath/webjars/WebJarsLocatorJSGenerator.java
@@ -3,14 +3,26 @@ package net.jawr.web.resource.bundle.generator.classpath.webjars;
 import net.jawr.web.resource.bundle.generator.resolver.ResourceGeneratorResolver;
 import net.jawr.web.resource.bundle.generator.resolver.WebJarsLocatorPathResolver;
 
+/**
+ * This class defines the generator for webjars JS resources which use the
+ * WebJarsLocator. So to reference a ressource you only need the short path
+ * reference instead of the full path one. <br/>
+ * For example : webjars:jquery.js instead of webjars:/jquery/2.1.4/jquery.js
+ *
+ * @author Ibrahim Chaehoi
+ */
 public class WebJarsLocatorJSGenerator extends WebJarsJSGenerator {
 
-    /* (non-Javadoc)
-     * @see net.jawr.web.resource.bundle.generator.classpath.ClasspathJSGenerator#createResolver(java.lang.String)
-     */
-    @Override
-    protected ResourceGeneratorResolver createResolver(String generatorPrefix) {
-        return new WebJarsLocatorPathResolver(generatorPrefix);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.bundle.generator.classpath.ClasspathJSGenerator
+	 * #createResolver(java.lang.String)
+	 */
+	@Override
+	protected ResourceGeneratorResolver createResolver(String generatorPrefix) {
+		return new WebJarsLocatorPathResolver(generatorPrefix, true, false);
+	}
 
 }
