fb414c4e56277092f085eb8719fb629acaef0ae2
Ibrahim Chaehoi <ichaehoi@gmail.com>
2015-08-19 13:30:38 +0200

    Fix issue JAWR-349 Allow user to use short reference for webjars resources instead of full one Add support for specifying webjars parameter to look into for resource search

--- jawr/jawr-core/src/main/java/net/jawr/web/resource/bundle/generator/classpath/webjars/WebJarsLocatorJSGenerator.java
+++ jawr/jawr-core/src/main/java/net/jawr/web/resource/bundle/generator/classpath/webjars/WebJarsLocatorJSGenerator.java
@@ -8,7 +8,11 @@ import net.jawr.web.resource.bundle.generator.resolver.WebJarsLocatorPathResolve
  * WebJarsLocator. So to reference a ressource you only need the short path
  * reference instead of the full path one. <br/>
  * For example : webjars:jquery.js instead of webjars:/jquery/2.1.4/jquery.js
+ * To avoid resource reference collision if there multiple resource with the same name in different webjars,
+ * like below :</br>
+ * webjars:/jquery.js[jquery]
  *
+ * @author (Original) Ted Liang (https://github.com/tedliang)
  * @author Ibrahim Chaehoi
  */
 public class WebJarsLocatorJSGenerator extends WebJarsJSGenerator {
