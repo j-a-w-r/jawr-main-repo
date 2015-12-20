8037174eeb2a7a8fcf880ce40641683bf6d8adab
Ibrahim Chaehoi <ichaehoi@gmail.com>
2015-12-20 22:32:54 +0100

    Issue #JAWR-372 Integration test for external bundle

---
+++ jawr/jawr-integration-test/src/test/java/net/jawr/web/test/MainPageWithExternalBundleTest.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package net.jawr.web.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.JavaScriptPage;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlScript;
+
+/**
+ * Test case for standard page in production mode.
+ * 
+ * @author ibrahim Chaehoi
+ */
+@JawrTestConfigFiles(webXml = "net/jawr/web/standard/config/web.xml", jawrConfig = "net/jawr/web/standard/config/jawr-with-external-bundle.properties")
+public class MainPageWithExternalBundleTest extends MainPageTest {
+
+	/**
+	 * Creates the web client
+	 * 
+	 * @return the web client
+	 */
+	protected WebClient createWebClient() {
+
+		WebClient webClient = super.createWebClient();
+		return webClient;
+	}
+
+	/**
+	 * Returns the page URL to test
+	 * 
+	 * @return the page URL to test
+	 */
+	protected String getPageUrl() {
+		return getServerUrlPrefix() + getUrlPrefix()
+				+ "/index-with-external-bundle.jsp";
+	}
+
+	@Test
+	public void testPageLoad() throws Exception {
+
+		final List<String> expectedAlerts = Collections
+				.singletonList("A little message retrieved from the message bundle : Hello $ world!");
+		assertEquals(expectedAlerts, collectedAlerts);
+
+		assertContentEquals(
+				"/net/jawr/web/standard/resources/index-with-external-bundle-jsp-result-expected.txt",
+				page);
+	}
+
+	@Test
+	public void checkGeneratedJsLinks() {
+		// Test generated Script link
+		final List<HtmlScript> scripts = getJsScriptTags();
+		assertEquals(2, scripts.size());
+		HtmlScript script = scripts.get(0);
+		assertEquals("http://mycomp/js/foo.min.js",
+				script.getSrcAttribute());
+
+		script = scripts.get(1);
+		assertEquals(getUrlPrefix() + "/690372103.en_US/js/bundle/msg.js",
+				script.getSrcAttribute());
+	}
+
+	@Test
+	public void testJsBundleContent() throws Exception {
+
+		final List<HtmlScript> scripts = getJsScriptTags();
+		final HtmlScript script = scripts.get(1);
+		final JavaScriptPage page = getJavascriptPage(script);
+		assertContentEquals("/net/jawr/web/standard/resources/msg-bundle.js",
+				page);
+	}
+
+}
