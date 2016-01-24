59d1b7043887f1da82da93706bb682e6c82cb4b3
Ibrahim Chaehoi <ichaehoi@gmail.com>
2016-01-24 17:13:22 +0100

    Fix issue #JAWR-313
    Update integration-test

--- jawr/jawr-integration-test/src/test/java/net/jawr/web/test/smartbundling/MainPageTestWatcher.java
+++ jawr/jawr-integration-test/src/test/java/net/jawr/web/test/smartbundling/MainPageTestWatcher.java
@@ -3,14 +3,10 @@
  */
 package net.jawr.web.test.smartbundling;
 
-import static org.junit.Assert.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 
-import net.jawr.web.config.jmx.JawrConfigManagerMBean;
 import net.jawr.web.test.JawrTestConfigFiles;
-import net.jawr.web.test.jmx.JawrJmxClient;
 
 /**
  * Test case for standard page in production mode.
@@ -23,32 +19,19 @@ public class MainPageTestWatcher extends MainPageTest {
 	protected static String getTempFolder() {
 		return "jawr-integration-smartbundling-test-watch-1";
 	}
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see net.jawr.web.test.smartbundling.MainPageTest#updateContent()
 	 */
 	@Override
 	protected void updateContent() throws Exception, InterruptedException, FileNotFoundException, IOException {
 
 		updateResources();
-		
+
 		// Wait a little bit
-		Thread.sleep(300);
-		 
-		JawrJmxClient jmxClient = new JawrJmxClient();
-		JawrConfigManagerMBean cssMBean = jmxClient.getCssMbean();
-		
-		List<String> bundles = cssMBean.getDirtyBundleNames();
-		assertEquals(1, bundles.size());
-		assertEquals("component", bundles.get(0));
-		
-		JawrConfigManagerMBean jsMBean = jmxClient.getJsMbean();
-		bundles = jsMBean.getDirtyBundleNames();
-		assertEquals(1, bundles.size());
-		assertEquals("msg", bundles.get(0));
-		
-		jmxClient.getApplicationMBean().rebuildDirtyBundles();
+		Thread.sleep(3000);
 	}
-	
+
 }
