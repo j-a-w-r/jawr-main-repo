26676cb8cef873969f3fcc8d3aa76b625f77bd24
Ibrahim Chaehoi <ichaehoi@gmail.com>
2016-02-26 19:24:58 +0100

    Fix compilation issue with grails plugin

--- jawr-grails/jawr-grails-plugin/src/java/net/jawr/web/resource/handler/reader/grails/GrailsServletContextResourceReader.java
+++ jawr-grails/jawr-grails-plugin/src/java/net/jawr/web/resource/handler/reader/grails/GrailsServletContextResourceReader.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2015 Ibrahim Chaehoi
+ * Copyright 2010-2016 Ibrahim Chaehoi
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -32,6 +32,7 @@ import net.jawr.web.JawrGrailsConstant;
 import net.jawr.web.config.JawrConfig;
 import net.jawr.web.exception.BundlingProcessException;
 import net.jawr.web.exception.InvalidPathException;
+import net.jawr.web.resource.bundle.JoinableResourceBundle;
 import net.jawr.web.resource.bundle.factory.util.RegexUtil;
 import net.jawr.web.resource.bundle.locale.GrailsLocaleUtils;
 import net.jawr.web.resource.handler.reader.BaseServletContextResourceReader;
@@ -40,158 +41,176 @@ import net.jawr.web.util.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
- * This class defines the resource reader for Grails application which is based on the servletContextReader.
+ * This class defines the resource reader for Grails application which is based
+ * on the servletContextReader.
  * 
  * @author Ibrahim Chaehoi
  */
-public class GrailsServletContextResourceReader extends
-	BaseServletContextResourceReader {
+public class GrailsServletContextResourceReader extends BaseServletContextResourceReader {
 
 	/** The logger */
 	private static final Logger LOGGER = LoggerFactory.getLogger(GrailsServletContextResourceReader.class);
-	
+
 	/** The grails-app directory name */
 	private static final String GRAILS_APP_DIR = "grails-app";
-	
+
 	/** The pattern of the plugin resource path */
 	private static Pattern PLUGIN_RESOURCE_PATTERN = Pattern.compile("^(/plugins/([a-zA-Z0-9_\\-\\.]*))");
-	
+
 	/** The plugin map attribute name */
 	private String pluginMapAttributeName;
 
 	/** The plugin path map */
 	private Map<String, String> pluginPathMap;
-	
+
 	/** The flag indicating if the grails war is deployed */
 	private boolean warDeployed;
-	
+
 	/**
 	 * Constructor
 	 */
 	public GrailsServletContextResourceReader() {
 		this(JawrGrailsConstant.JAWR_GRAILS_PLUGIN_PATHS);
 	}
-	
+
 	/**
 	 * Constructor
 	 */
 	public GrailsServletContextResourceReader(String pluginMapAttributeName) {
 		this.pluginMapAttributeName = pluginMapAttributeName;
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.BaseServletContextResourceReader#init(javax.servlet.ServletContext, net.jawr.web.config.JawrConfig)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.handler.reader.BaseServletContextResourceReader#
+	 * init(javax.servlet.ServletContext, net.jawr.web.config.JawrConfig)
 	 */
 	@SuppressWarnings("unchecked")
-	public void init(ServletContext context,
-			JawrConfig config) {
+	public void init(ServletContext context, JawrConfig config) {
 		super.init(context, config);
 		pluginPathMap = (Map<String, String>) context.getAttribute(pluginMapAttributeName);
-		if(pluginPathMap == null){
+		if (pluginPathMap == null) {
 			throw new BundlingProcessException("No grails plugin paths map defined in the servlet context");
 		}
-		
-		// Determine wether this is run-app or run-war style of runtime. 
+
+		// Determine wether this is run-app or run-war style of runtime.
 		warDeployed = ((Boolean) context.getAttribute(JawrGrailsConstant.GRAILS_WAR_DEPLOYED)).booleanValue();
 	}
 
 	/**
-	 * Handle the mapping of the resource path to the right one.
-	 * This can be the case for plugin resources.
-	 * It will returns the file system path or the real path 
-	 * or the same path if the path has not been remapped
+	 * Handle the mapping of the resource path to the right one. This can be the
+	 * case for plugin resources. It will returns the file system path or the
+	 * real path or the same path if the path has not been remapped
 	 * 
-	 * @param path the path
+	 * @param path
+	 *            the path
 	 * @return the real path or the same path if the path has not been remapped
 	 */
-	public String getRealResourcePath(String path){
+	public String getRealResourcePath(String path) {
 		String realPath = path;
 		Matcher matcher = PLUGIN_RESOURCE_PATTERN.matcher(path);
 		StringBuffer sb = new StringBuffer();
-		
-		if(matcher.find()){
-			
+
+		if (matcher.find()) {
+
 			String pluginName = matcher.group(2);
 			String pluginPath = (String) pluginPathMap.get(pluginName);
-			if(pluginPath != null){
+			if (pluginPath != null) {
 				matcher.appendReplacement(sb, RegexUtil.adaptReplacementToMatcher(pluginPath));
 				matcher.appendTail(sb);
 				realPath = sb.toString();
-				if(LOGGER.isDebugEnabled()){
-					LOGGER.debug("Plugin path '"+path+"' mapped to '"+realPath+"'");
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("Plugin path '" + path + "' mapped to '" + realPath + "'");
 				}
-			}else{
-				if(LOGGER.isDebugEnabled()){
-					LOGGER.debug("No Plugin path found for '"+pluginName);
+			} else {
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("No Plugin path found for '" + pluginName);
 				}
 			}
 		}
-		
+
 		return realPath;
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#getResource(java.lang.String, boolean)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.handler.reader.BaseServletContextResourceReader#
+	 * getResource(net.jawr.web.resource.bundle.JoinableResourceBundle,
+	 * java.lang.String, boolean)
 	 */
-	@SuppressWarnings("resource")
-	public Reader getResource(String resourceName, boolean processingBundle) {
-		
+	@Override
+	public Reader getResource(JoinableResourceBundle bundle, String resourceName, boolean processingBundle) {
+
 		Reader rd = null;
 		String realPath = getRealResourcePath(resourceName);
-		if(isFileSystemPath(resourceName, realPath)){ // The resource has been remapped
+		if (isFileSystemPath(resourceName, realPath)) { // The resource has been
+														// remapped
 			try {
 				rd = new FileReader(realPath);
 			} catch (FileNotFoundException e) {
-				LOGGER.debug("The resource "+realPath+" has not been found");
+				LOGGER.debug("The resource " + realPath + " has not been found");
 			}
 		}
-		if(rd == null){
-			rd = super.getResource(realPath, processingBundle);
+		if (rd == null) {
+			rd = super.getResource(bundle, realPath, processingBundle);
 		}
-		
+
 		return rd;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#getResource(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.jawr.web.resource.handler.reader.BaseServletContextResourceReader#
+	 * getResource(net.jawr.web.resource.bundle.JoinableResourceBundle,
+	 * java.lang.String)
 	 */
-	@SuppressWarnings("resource")
-	public Reader getResource(String resourceName) {
+	@Override
+	public Reader getResource(JoinableResourceBundle bundle, String resourceName) {
 		Reader rd = null;
 		String realPath = getRealResourcePath(resourceName);
-		if(isFileSystemPath(resourceName, realPath)){ // The resource has been remapped to a file system path
+		if (isFileSystemPath(resourceName, realPath)) { // The resource has been
+														// remapped to a file
+														// system path
 			try {
 				rd = new FileReader(realPath);
 			} catch (FileNotFoundException e) {
-				LOGGER.debug("The resource "+realPath+" has not been found");
+				LOGGER.debug("The resource " + realPath + " has not been found");
 			}
 		}
-		if(rd == null){
-			rd = super.getResource(realPath);
+		if (rd == null) {
+			rd = super.getResource(bundle, realPath);
 		}
-		
+
 		return rd;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#getResourceAsStream(java.lang.String, boolean)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#
+	 * getResourceAsStream(java.lang.String, boolean)
 	 */
-	@SuppressWarnings("resource")
-	public InputStream getResourceAsStream(String resourceName,
-			boolean processingBundle) {
-		
+	public InputStream getResourceAsStream(String resourceName, boolean processingBundle) {
+
 		InputStream is = null;
 		String realPath = getRealResourcePath(resourceName);
-		if(isFileSystemPath(resourceName, realPath)){ // The resource has been remapped to a file system path
+		if (isFileSystemPath(resourceName, realPath)) { // The resource has been
+														// remapped to a file
+														// system path
 			try {
 				is = new FileInputStream(realPath);
 			} catch (FileNotFoundException e) {
-				LOGGER.debug("The resource "+realPath+" has not been found");
+				LOGGER.debug("The resource " + realPath + " has not been found");
 			}
 		}
-		if(is == null){
+		if (is == null) {
 			is = super.getResourceAsStream(realPath, processingBundle);
 		}
 		return is;
@@ -199,73 +218,90 @@ public class GrailsServletContextResourceReader extends
 
 	/**
 	 * Checks if the resource should be accessible
-	 * @param resourceName the resource name
+	 * 
+	 * @param resourceName
+	 *            the resource name
 	 * @return true if the resource should be accessible
 	 */
-	protected boolean isAccessPermitted(String resourceName){
-	
+	protected boolean isAccessPermitted(String resourceName) {
+
 		boolean accessible = true;
-		if(resourceName.startsWith(JawrConstant.WEB_INF_DIR_PREFIX)){
-			if(resourceName.startsWith(JawrConstant.WEB_INF_DIR_PREFIX+GRAILS_APP_DIR) && resourceName.endsWith(GrailsLocaleUtils.MSG_RESOURCE_BUNDLE_SUFFIX)){
+		if (resourceName.startsWith(JawrConstant.WEB_INF_DIR_PREFIX)) {
+			if (resourceName.startsWith(JawrConstant.WEB_INF_DIR_PREFIX + GRAILS_APP_DIR)
+					&& resourceName.endsWith(GrailsLocaleUtils.MSG_RESOURCE_BUNDLE_SUFFIX)) {
 				accessible = true;
-			}else{
+			} else {
 				accessible = false;
 			}
 		}
-		
-		return accessible; 
+
+		return accessible;
 	}
-	
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#getResourceAsStream(java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#
+	 * getResourceAsStream(java.lang.String)
 	 */
-	@SuppressWarnings("resource")
 	public InputStream getResourceAsStream(String resourceName) {
 		InputStream is = null;
 		String realPath = getRealResourcePath(resourceName);
-		if(isFileSystemPath(resourceName, realPath)){ // The resource has been remapped to a file system path
-				try {
+		if (isFileSystemPath(resourceName, realPath)) { // The resource has been
+														// remapped to a file
+														// system path
+			try {
 				is = new FileInputStream(realPath);
 			} catch (FileNotFoundException e) {
-				LOGGER.debug("The resource "+realPath+" has not been found");
+				LOGGER.debug("The resource " + realPath + " has not been found");
 			}
 		}
-		if(is == null){
+		if (is == null) {
 			is = super.getResourceAsStream(realPath);
 		}
 		return is;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#getResourceNames(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#
+	 * getResourceNames(java.lang.String)
 	 */
 	public Set<String> getResourceNames(String path) {
-		
+
 		Set<String> resourceNames = new HashSet<String>();
 		String realPath = getRealResourcePath(path);
-		if(isFileSystemPath(path, realPath)){ // The resource has been remapped to a file system path
-			try{
+		if (isFileSystemPath(path, realPath)) { // The resource has been
+												// remapped to a file system
+												// path
+			try {
 				resourceNames = FileUtils.getResourceNames(new File(realPath));
-			}catch(InvalidPathException e){
-				LOGGER.warn("The path "+path+" which has been mapped to "+realPath+" doesn't exist");
+			} catch (InvalidPathException e) {
+				LOGGER.warn("The path " + path + " which has been mapped to " + realPath + " doesn't exist");
 			}
-		}else{
+		} else {
 			resourceNames = super.getResourceNames(realPath);
 		}
-		
+
 		return resourceNames;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#isDirectory(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.jawr.web.resource.handler.reader.ServletContextResourceReader#
+	 * isDirectory(java.lang.String)
 	 */
 	public boolean isDirectory(String path) {
-		
+
 		boolean isDirectory = false;
 		String realPath = getRealResourcePath(path);
-		if(isFileSystemPath(path, realPath)){ // The resource has been remapped to a file system path
+		if (isFileSystemPath(path, realPath)) { // The resource has been
+												// remapped to a file system
+												// path
 			isDirectory = new File(realPath).isDirectory();
-		}else{
+		} else {
 			isDirectory = super.isDirectory(realPath);
 		}
 		return isDirectory;
@@ -273,23 +309,29 @@ public class GrailsServletContextResourceReader extends
 
 	/**
 	 * Checks if the path given in parameter are file system path or not
-	 * @param path the original path
-	 * @param realPath the real path
+	 * 
+	 * @param path
+	 *            the original path
+	 * @param realPath
+	 *            the real path
 	 * @return true if the path given in parameter are file system path or not
 	 */
 	public boolean isFileSystemPath(String path) {
-		
+
 		return isFileSystemPath(getRealResourcePath(path), path);
 	}
-	
+
 	/**
 	 * Checks if the path given in parameter are file system path or not
-	 * @param path the original path
-	 * @param realPath the real path
+	 * 
+	 * @param path
+	 *            the original path
+	 * @param realPath
+	 *            the real path
 	 * @return true if the path given in parameter are file system path or not
 	 */
 	private boolean isFileSystemPath(String realPath, String path) {
-		
+
 		return !warDeployed && new File(realPath).exists();
 	}
 
